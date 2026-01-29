652dc177bcc2c6d245428a0aba08b5d9c670cc63
elmimkt-bit <elmimkt@gmail.com>
2026-01-29 16:16:28 +0800

    Comment out server port and listener
    Comment out the PORT variable and server listener.

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello world!");
 });
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log("Server is running on port 3000"));
+//server.listen(PORT, () => console.log("Server is running on port 3000"));
